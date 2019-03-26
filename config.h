a8c40b93be0aa3fe1ab59b10e8ca3cf98d9a42e3
Etienne Beaulieu <ety_95@hotmail.fr>
2019-03-25 20:37:10 -0400

    Add pywal support

--- config.h
+++ config.h
@@ -66,8 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "/home/etienne/.scripts/dmenu.sh", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "/home/etienne/.scripts/dmenu.sh", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *fmcmd[] = { "st", "-e", "nnn", NULL };
 static const char *emacscmd[] = { "emacs", NULL };
