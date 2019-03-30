4085a6845320a9905f0112e2da3ed86a596e58fa
Etienne Beaulieu <ety_95@hotmail.fr>
2019-03-30 11:30:57 -0400

    Cancel URXVT

--- config.h
+++ config.h
@@ -28,7 +28,7 @@ static const char *colors[][3]      = {
 #define AudioMute			0x1008ff12 
 
 
-static const char term[] = "urxvt";
+static const char term[] = "st";
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
